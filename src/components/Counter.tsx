0da624ef52cee1b991d4c6f6b46721cf1403aa69
yuta-jeng <yuta.jeng@gmail.com>
2021-08-18 14:49:36 +0900

    8th end2

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Counter = () => {
     // useStateで状態取得
@@ -19,9 +19,26 @@ const Counter = () => {
         setCount((prevState) => prevState - 1)
     }
 
+    // useEffectでライフサイクル処理！
+    // コンポーネントが再レンダリングされるたびに呼び出される
+    // 最初のレンダリングでも呼び出される
+    useEffect(() => {
+        console.log('再レンダリングされましたよ～', count)
+    })
+
+    // deps(依存関係)が初回のみ
+    useEffect(() => {
+        console.log('最初のレンダリングですよ～', count)
+    }, [])
+
+    // deps(依存関係)がcountが変更されるたび
+    useEffect(() => {
+        console.log('カウント1が変更されましたよ～', count)
+    }, [count])
+
     return (
         <div>
-            <p>現在のカウント値: {count}</p>
+            <p>カウントの値: {count}</p>
             <button onClick={countUp} type="button">
                 up
             </button>
--- src/components/ToggleButton.tsx
+++ src/components/ToggleButton.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const ToggleButton = () => {
     const [open, setOpen] = useState(false)
@@ -7,6 +7,20 @@ const ToggleButton = () => {
     const toggle = () => {
         setOpen((prevState) => !prevState)
     }
+
+    useEffect(() => {
+        console.log('今のトグルの状態は...', open)
+        if (open) {
+            console.log('サブスク！...')
+        }
+
+        // クリーンアップ関数
+        // 再レンダリングされる前に呼び出される！！
+        // Unmountの時に実行されるという意味
+        return () => {
+            console.log('サブスク終了…')
+        }
+    })
 
     return (
         <button onClick={toggle} type="button">
